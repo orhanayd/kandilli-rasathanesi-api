ad488e7922c02d0cc4ab0b76525c4d900065571c
Orhan AYDOĞDU <info@orhanaydogdu.com.tr>
2023-02-21 01:43:50 +0300

    20230221
    + döküman güncellendi.

--- src/routes/kandilli/index.js
+++ src/routes/kandilli/index.js
@@ -17,6 +17,7 @@ router.get('/live', [controller.kandilli.live], services.kandilli.live);
  * GET /deprem/kandilli/archive
  * @param {number} limit.query - limit param limit
  * @param {string} date.query.required - date param YYYY-MM-DD
+ * @param {string} date_end.query - date end param YYYY-MM-DD
  * @summary api earthquakes archive endpoint from kandilli
  * @tags KANDILLI
  * @return {object} 200 - success response - application/json
